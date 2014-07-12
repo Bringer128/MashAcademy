2447b8852628f31d9150a9fae61ae87efa6dc6cc
Chris Bellew <cjbellew@gmail.com>
2014-07-12 11:19:44 +0800

    Removed Christmas Island

--- scripts/map.js
+++ scripts/map.js
@@ -22,32 +22,39 @@ angular.module('MashAcademy')
 			
 			var feature = topojson.feature(json, json.objects.layer1);
 			
-			var width = $element.width(),
-			height = $element.height();
-
-			var svg = d3.select($element.find('svg').get(0))
-				.attr("width", width)
-				.attr("height", height);
-				
-			var path = d3.geo.path().projection(d3.geo.mercator());
-			var svgPath = svg.append("path")
-				.datum(feature)
-				.attr('fill', 'white')
-				.attr("d", path)
-				
-			var bounds = path.bounds(feature),
-				dx = bounds[1][0] - bounds[0][0],
-				dy = bounds[1][1] - bounds[0][1],
-				x = (bounds[0][0] + bounds[1][0]) / 2,
-				y = (bounds[0][1] + bounds[1][1]) / 2,
-				scale = .9 / Math.max(dx / width, dy / height),
-				translate = [width / 2 - scale * x, height / 2 - scale * y];
-				
-			svgPath.attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+			render();
 			
-			cities.forEach(function(city) {
+			$(window).resize(render);
+			
+			function render() {
+				var width = $element.width(),
+				height = $element.height();
+
+				$element.find('svg').empty();
+				var svg = d3.select($element.find('svg').get(0))
+					.attr("width", width)
+					.attr("height", height);
+					
+				var path = d3.geo.path().projection(d3.geo.mercator());
+				var svgPath = svg.append("path")
+					.datum(feature)
+					.attr('fill', 'white')
+					.attr("d", path)
+					
+				var bounds = path.bounds(feature),
+					dx = bounds[1][0] - bounds[0][0],
+					dy = bounds[1][1] - bounds[0][1],
+					x = (bounds[0][0] + bounds[1][0]) / 2,
+					y = (bounds[0][1] + bounds[1][1]) / 2,
+					scale = .9 / Math.max(dx / width, dy / height),
+					translate = [width / 2 - scale * x, height / 2 - scale * y];
+					
+				svgPath.attr("transform", "translate(" + translate + ")scale(" + scale + ")");
 				
-			});
+				cities.forEach(function(city) {
+					
+				});
+			}
 		});
 	}
   }}]);
