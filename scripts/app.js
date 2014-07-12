44d1fddea28c4aee372c9fcca6bc8648fb6f41cf
Chris Bellew <cjbellew@gmail.com>
2014-07-12 16:42:19 +0800

    Added question marking

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,39 @@
 var mashAcademy = angular.module('MashAcademy', ['vr.directives.slider'])
-.controller('MashAcademy', function($scope) {
+.controller('MashAcademy', function($scope, $timeout) {
 	$scope.questions = [
-		//{ question:  }
+		{ question: 'What was the MINIMUM TEMPERATURE in Broome on the 18th June 2014?', answer: 8.3, type: 'text', marker: markTextQuestion, hint: 'Try using the temperature data and putting it on the map!', passed: false },
+		{ question: 'Was it SUNNY, CLOUDY or RAINING in Cairns on the 1st July 2014?', answer: 'Sunny', type: 'choice', marker: markTextQuestion, options: ['Sunny', 'Cloudy', 'Raining'], hint: 'Try using the weather data and putting it on the map!', passed: false },
+		{ question: 'WHERE was the highest maximum temperature on the 6th May 2014?', answer: 'Broome', type: 'text', marker: markTextQuestion, hint: 'Try using the temperature data and putting it on the map!', passed: false },
 	];
+	
+	$scope.currentQuestionIndex = 0;
+	$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+	
+	$scope.nextQuestion = function() {
+		$scope.currentQuestionIndex++;
+		$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+	}
+	
+	$scope.previousQuestion = function() {
+		$scope.currentQuestionIndex--;
+		$scope.currentQuestion = $scope.questions[$scope.currentQuestionIndex];
+	}
+	
+	function markTextQuestion(answer) {
+		if ((answer || '').toString().toLowerCase() == ($scope.currentQuestion.answer || '').toString().toLowerCase() ) {
+			$scope.currentQuestion.passed = true;
+		}
+		
+		showError();
+	}
+	
+	function showError() {
+		$scope.errorShowing = true;
+		
+		$timeout(function() {
+			$scope.errorShowing = false;
+		}, 1000);
+	}
 })
 .run(function ($rootScope) {
     $rootScope.fireResize = function () {
