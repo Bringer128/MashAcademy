12155898e3383b12ef460a41b1ce988b898fe509
Ryan Musgrave <ryanm128@gmail.com>
2014-07-12 09:46:28 +0800

    Adding some images

--- scripts/toolbox.js
+++ scripts/toolbox.js
@@ -1,6 +1,21 @@
 angular.module('MashAcademy')
 .directive('toolbox', [function() {
   return {
-	  templateUrl: 'templates/toolbox.html'
+	restrict: 'E',
+	templateUrl: 'templates/toolbox.html',
+	replace: false,
+    scope: false,
+	link: function($scope, $element, $attrs) {
+	    $scope.controls = [
+
+	    ];
+	    $scope.data = [{
+	        name: 'weather',
+            icon: 'content/sun-clouds.png'
+	    }, {
+	        name: 'temperature',
+            icon: 'content/degrees.gif'
+	    }];
+	}
   };
 });
