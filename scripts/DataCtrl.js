11f141b1ef5bdcb354d8f0a100a8e72f70645ca0
Ryan Musgrave <ryanm128@gmail.com>
2014-07-12 17:17:22 +0800

    Added DataCtrl.js

---
+++ scripts/DataCtrl.js
@@ -0,0 +1,43 @@
+angular.module('MashAcademy')
+.controller('DataCtrl', function ($scope, $http) {
+    
+    $scope.temperatures = {
+        "Perth": [
+            ["2014-01-01", 10],
+            ["2014-01-02", 20],
+            ["2014-01-03", 30]
+        ]
+    };
+    $scope.currentTemperature = function (city) {
+        return $scope.temperatures[city].reduce(function (m, e) {
+            if (m) return m;
+
+            var date = Date.parse(e[0]);
+
+            if (date.getTime() == $scope.sliderValue) {
+                return e[1];
+            }
+        });
+    };
+
+    $scope.times = Object.keys($scope.temperatures).map(function (e) {
+        return $scope.temperatures[e];
+    }).reduce(function (joined, arr) {
+        return joined.concat(arr);
+    }, []).map(function (element) {
+        return Date.parse(element[0]);
+    });
+
+    $scope.maxTime = $scope.times.reduce(toMax);
+    $scope.minTime = $scope.times.reduce(toMin);
+
+    function toMax(m, e) {
+        return m > e ? m : e;
+    }
+    function toMin(m, e) {
+        return m < e ? m : e;
+    }
+    function dateToMSecs(date) {
+        return date.getTime();
+    }
+});
